51393d5b174b88d92b8c072f1c80d81aefe61e9d
BaylorDeng <baylor4mailbox@gmail.com>
2025-03-03 01:29:15 +0800

    Update Lab1_3.java

--- CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_3.java
+++ CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_3.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class Lab1_3 {
     public static void main(String[] args) {
-        // Always using println to print everything
+        // The following is return statement, you may directly copy and paste to your main method.
+        // System.out.println("Invalid Input");
+        // System.out.println("Today is " + (...) + " and the future day is " + (...);
         // >>>>>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<<<<<<
         
         
