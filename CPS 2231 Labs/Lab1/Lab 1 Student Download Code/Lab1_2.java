f287b38a296d5e024b54bd7b73c472793c02db39
BaylorDeng <baylor4mailbox@gmail.com>
2025-03-03 01:27:29 +0800

    Update Lab1_2.java

--- CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_2.java
+++ CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Lab1_2 {
     // Add method declaration by yourself, don't change the method name
     isPalindrome(String s) {
+        // return true or false in this question
         // >>>>>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<<<<<<
         
         
