6777d5da5e2a4bc1c7b31872214e3e19f1d5ab47
BaylorDeng <baylor4mailbox@gmail.com>
2025-03-03 01:31:42 +0800

    Update Lab1_4.java

--- CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_4.java
+++ CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_4.java
@@ -4,6 +4,11 @@ package Lab1;
 public class Lab1_4 {
     public static void main(String[] args) {
     // Always using println to print everything
+    // Make sure the last output is result
+    // You can use following statement as output, you can copy directly:
+    // System.out.println("r2 is inside r1");
+    // System.out.println("r2 does not overlap r1");
+    // System.out.println("Invalid Input");
     // >>>>>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<<<<<<
         
         
