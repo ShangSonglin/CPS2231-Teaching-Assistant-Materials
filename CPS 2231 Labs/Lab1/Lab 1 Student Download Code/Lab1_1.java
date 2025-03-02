14e86e4b31af6746f2114584af59facc1980585a
BaylorDeng <baylor4mailbox@gmail.com>
2025-03-03 01:26:33 +0800

    Update Lab1_1.java

--- CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_1.java
+++ CPS 2231 Labs/Lab1/Lab 1 Student Download Code/Lab1_1.java
@@ -7,6 +7,9 @@ public class Lab1_1 {
     defangIPaddr(String address) {
         // In this part, you can't use any existing packages
         // For example, you can't use String.split() or String.replace()
+        // Return Result:(You might directly copy and paste following statement for invalid)
+        // return "Invalid IP address.";
+        // Or return final result.
         // >>>>>>>>>>>>>>> YOUR CODE HERE <<<<<<<<<<<<<<<
         
         
